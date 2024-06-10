45f1ccb5370fbf812f15316a10f0b248396eb7c4
suchomelova-dana <danka.suchomelova@seznam.cz>
2024-06-10 20:05:39 +0200

    deploy test

---
+++ scripts/gh-pages-deploy.js
@@ -0,0 +1,31 @@
+const execa = require("execa");
+const fs = require("fs");
+
+(async () => {
+    try {
+        await execa("git", ["checkout", "--orphan", "gh-pages"]);
+        //await execa("echo", ["sktrasko.cz", ">","CNAME"]);
+        console.log("Building...");
+        await execa("npm", ["run", "build"]);
+
+        // Understand if it's dist or build folder
+        const folderName = fs.existsSync("dist") ? "dist" : "build";
+
+        // Copy content of index.html to 404.html
+        const indexContent = fs.readFileSync(`${folderName}/index.html`, "utf8");
+        fs.writeFileSync(`${folderName}/404.html`, indexContent);
+
+        //fs.writeFileSync(`${folderName}/CNAME`, "sktrasko.cz");
+        await execa("git", ["--work-tree", folderName, "add", "--all"]);
+        await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
+        console.log("Pushing to gh-pages...");
+        await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
+        // await execa("rm", ["-r", folderName]);
+        await execa("git", ["checkout", "-f", "main"]);
+        await execa("git", ["branch", "-D", "gh-pages"]);
+        console.log("Successfully deployed");
+    } catch (e) {
+        console.log(e.message);
+        process.exit(1);
+    }
+})();
